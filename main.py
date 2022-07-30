38cc6dfe20204a22c9d007939b62ae047d1910fb
pavel <ponch092009@yandex.ru>
2022-07-30 11:59:33 +0300

    new req

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
+import io
 import time
+import zipfile
 
+import requests
 from pycaiso.oasis import Node
 from datetime import datetime
 import pandas as pd
 
 df = pd.read_csv('LMPLocations.csv')
 names_lst = df['name'].tolist()
+print(names_lst[0])
 # cj = Node(names_lst[0])
 # cj_lmps = cj.get_lmps(datetime(2022, 1, 1), datetime(2022, 1, 30))
-# # cj_lmps = cj.get_lmps(datetime(2021, 1, 1), datetime(2021, 1, 15))
-# # cj_lmps.to_csv('tst.csv')
-# print(cj_lmps.head())
+# cj_lmps = cj.get_lmps(datetime(2021, 1, 1), datetime(2021, 1, 15))
+# cj_lmps.to_csv('tst.csv')
+#print(cj_lmps.head())
 # cj = Node(name)
 # select pnode
 def ds():
-    for name in names_lst[0]:
+    for name in names_lst[3]:
         cj = Node(name)
         # create dataframe with LMPS from arbitrary period (30 day maximum).
         # time.sleep(10)
-        cj_lmps = cj.get_lmps(datetime(2022, 1, 1), datetime(2022 , 1, 15))
+        cj_lmps = cj.get_lmps(datetime(2022, 6, 1), datetime(2022 , 6, 2))
         # time.sleep(10)
         # cj_lmps2=cj.get_lmps(datetime(2022, 1, 15), datetime(2022, 1,29 ))
         # time.sleep(10)
@@ -34,7 +38,18 @@ def ds():
         # df_f.to_csv(f'outputs/{name}.csv')
 #     cj_lmps2 = cj.get_lmps(datetime(2022, 1, 31), datetime(2022, 2, 28))
 
-ds()
+#ds()
+def my_req():
+    rsp=requests.get('http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_LMP&startdatetime=20220729T15:00-0000&enddatetime=20220729T23:00-0000&version=1&market_run_id=DAM&node=MALCHA_7_B1&resultformat=6')
+    z = zipfile.ZipFile(io.BytesIO(rsp.content))
+    z.extractall("csv")
+my_req()
+
+def my_req_int():
+    rsp=requests.get('http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_LMP&startdatetime=20220729T15:00-0000&enddatetime=20220729T23:00-0000&version=1&market_run_id=DAM&node=MALCHA_7_B1&resultformat=6')
+    z = zipfile.ZipFile(io.BytesIO(rsp.content))
+    z.extractall("csv")
+my_req_int()
 # sp15 = Node.SP15()
 # sp15_lmps = sp15.get_lmps(datetime(2021, 1, 1), datetime(2021, 1, 2))
 # sp15_lmps.to_csv('tst.csv')
