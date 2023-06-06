7f83943eacd622da0a84407f8f1de9c3972cd81c
whiteforestever <danila.whiteman@gmail.com>
2023-06-06 22:21:14 +0300

    наговнокодил люто, устал, дальше мои полномочия все

--- main_v2.py
+++ main_v2.py
@@ -1,9 +1,14 @@
 import io
+import os
 import time
 import zipfile
-from datetime import datetime
+import datetime
 import requests
 import pandas as pd
+import logging
+from tqdm import tqdm
+
+logging.basicConfig(filename='logerrors.txt', level=logging.INFO)
 
 def get_prices(nodename, startdate, enddate):
     retry_count = 3
@@ -13,21 +18,32 @@ def get_prices(nodename, startdate, enddate):
         try:
             rsp = requests.get(
                 f'http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_INTVL_LMP&startdatetime={startdate}&enddatetime={enddate}&version=1&market_run_id=RTM&node={nodename}&resultformat=6',
-                timeout=335)
+                timeout=10)
 
             rsp.raise_for_status()
             z = zipfile.ZipFile(io.BytesIO(rsp.content))
             csv = z.open(z.namelist()[0])
             df = pd.read_csv(csv)
-            return df
-        except (requests.HTTPError, zipfile.BadZipFile) as e:
-            if isinstance(e, requests.HTTPError) and e.response.status_code == 429:
+        except (requests.HTTPError, zipfile.BadZipFile, requests.exceptions.Timeout) as e:
+            if isinstance(e, requests.exceptions.Timeout) or isinstance(e, requests.HTTPError) and e.response.status_code == 429:
                 time.sleep(retry_delay)
             else:
-                print(f"Error occurred: {str(e)}")
+                logging.error(f"request for {startdate}-{enddate} {nodename} in market_run_id=RTM: Error occurred: {str(e)}")
                 return None
+        except requests.exceptions.ConnectionError as e:
+            if 'ConnectionResetError' in str(e):
+                time.sleep(retry_delay)
+            else:
+                logging.error(f"request for {startdate}-{enddate} {nodename} in market_run_id=RTM: {str(e)}")
+                logging.error("Note: check that you are connecting via USA")
+                return None
+        except Exception as e:
+            logging.error(f"request for {startdate}-{enddate} {nodename} in market_run_id=RTM: Unhandled exception occurred: {str(e)}")
+            return None
+        else:
+            return df[["INTERVALSTARTTIME_GMT", "INTERVALENDTIME_GMT", "NODE", "MARKET_RUN_ID", "LMP_TYPE", "MW", "OPR_INTERVAL"]]
     
-    print("Exceeded maximum retry attempts")
+    logging.error(f"request for {startdate}-{enddate} {nodename} in market_run_id=RTM: Exceeded maximum retry attempts")
     return None
 
 def get_dam(nodename, startdate, enddate):
@@ -37,46 +53,68 @@ def get_dam(nodename, startdate, enddate):
     for _ in range(retry_count):
         try:
             rsp = requests.get(
-                f'http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_LMP&startdatetime={startdate}&enddatetime={enddate}&version=1&market_run_id=DAM&node={nodename}&resultformat=6')
+                f'http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_LMP&startdatetime={startdate}&enddatetime={enddate}&version=1&market_run_id=DAM&node={nodename}&resultformat=6',
+                timeout=10)
 
             rsp.raise_for_status()
             z = zipfile.ZipFile(io.BytesIO(rsp.content))
             csv = z.open(z.namelist()[0])
             df = pd.read_csv(csv)
-            return df
-        except (requests.HTTPError, zipfile.BadZipFile) as e:
-            if isinstance(e, requests.HTTPError) and e.response.status_code == 429:
+        except (requests.HTTPError, zipfile.BadZipFile, requests.exceptions.Timeout) as e:
+            if isinstance(e, requests.exceptions.Timeout) or isinstance(e, requests.HTTPError) and e.response.status_code == 429:
                 time.sleep(retry_delay)
             else:
-                print(f"Error occurred: {str(e)}")
+                logging.error(f"request for {startdate}-{enddate} {nodename} in market_run_id=RTM: Error occurred: {str(e)}")
                 return None
+        except requests.exceptions.ConnectionError as e:
+            if 'ConnectionResetError' in str(e):
+                time.sleep(retry_delay)
+            else:
+                logging.error(f"request for {startdate}-{enddate} {nodename} in market_run_id=RTM: {str(e)}")
+                logging.error("Note: check that you are connecting via USA")
+                return None
+        except Exception as e:
+            logging.error(f"request for {startdate}-{enddate} {nodename} in market_run_id=RTM: Unhandled exception occurred: {str(e)}")
+            return None
+        else:
+            return df[["INTERVALSTARTTIME_GMT", "INTERVALENDTIME_GMT", "NODE", "MARKET_RUN_ID", "LMP_TYPE", "MW"]]
     
-    print("Exceeded maximum retry attempts")
+    logging.error(f"request for {startdate}-{enddate} {nodename} in market_run_id=RTM: Exceeded maximum retry attempts")
     return None
 
 
 if __name__ == '__main__':
     df = pd.read_csv('LMPLocations.csv')
     names_lst = df['name'].tolist()
-    current_date = datetime.datetime(2022, 8, 16, 9, 0) # время условное поставил для проверки,
-    end_date = datetime.datetime(2022, 8, 25, 0, 0) # так-то можно поставить двухлетней давности и должно работать, просто долго
+    start_date = datetime.datetime(2022, 4, 30, 9, 0)
+    end_date = datetime.datetime(2022, 5, 7, 0, 0)
+    dates_list = list(start_date + datetime.timedelta(days=i) for i in range((end_date - start_date).days + 1))
+    # dates_list = list(start_date + datetime.timedelta(days=7*i) for i in range((end_date - start_date).days // 7 + 1)) # если по неделям
+    folder_path = 'data'
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
 
-    while current_date <= end_date:
-        date_str = current_date.strftime('%Y%m%d')
-        
-        for node_name in names_lst:
-            start_time = current_date.strftime('%Y%m%dT%H:%M-0000')
-            end_time = (current_date + datetime.timedelta(hours=15)).strftime('%Y%m%dT%H:%M-0000') # +15 часов, так как там данные с 9 утра до 00 след дня
+    for node_name in names_lst:
+        df_rtm = pd.DataFrame(columns=["INTERVALSTARTTIME_GMT", "INTERVALENDTIME_GMT", "NODE", "MARKET_RUN_ID", "LMP_TYPE", "MW", "OPR_INTERVAL"])
+        df_dam = pd.DataFrame(columns=["INTERVALSTARTTIME_GMT", "INTERVALENDTIME_GMT", "NODE", "MARKET_RUN_ID", "LMP_TYPE", "MW"])
+        for i in tqdm(range(len(dates_list)-1)):
+            start_time = dates_list[i].strftime('%Y%m%dT%H:%M-0000')
+            end_time = dates_list[i+1].strftime('%Y%m%dT%H:%M-0000')
             
             df = get_prices(node_name, start_time, end_time)
             if df is not None:
-                df.to_csv(f'data/{node_name}-RTM-{date_str}.csv')
+                df_rtm = pd.concat([df_rtm, df], ignore_index=True)
             
-            # я нашел на сайте только для market_run_id=RTM (get_prices), поэтому возможно это не нужно
-            # да и я почитал, RTM -- рынок, на котором определяются цены на энергию и осуществляются сделки в реальном времени.
-            # т.е. получаем цены на энергию с рынка реального времени для указанного периода времени и местоположения.
             df = get_dam(node_name, start_time, end_time)
             if df is not None:
-                df.to_csv(f'data/{node_name}-DAM-{date_str}.csv')
+                df_dam = pd.concat([df_dam, df], ignore_index=True)
         
-        current_date += datetime.timedelta(days=1)
+        if df_rtm.shape[0] > 0:
+            file_path = f'{folder_path}/{node_name}-RTM-{start_date.strftime("%Y%m%d")}-{end_date.strftime("%Y%m%d")}.csv'
+            with open(file_path, 'w', newline='') as file:
+                df_rtm.to_csv(file, index=False)
+        
+        if df_dam.shape[0] > 0:
+            file_path = f'{folder_path}/{node_name}-DAM-{start_date.strftime("%Y%m%d")}-{end_date.strftime("%Y%m%d")}.csv'
+            with open(file_path, 'w', newline='') as file:
+                df_dam.to_csv(file, index=False)
